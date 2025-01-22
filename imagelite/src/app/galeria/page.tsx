4d182407afd3226a4c7ec393941aea25c0f435f8
William Mian <william_mian@yahoo.com.br>
2025-01-22 11:00:46 -0300

    componentes Template e ImageCard

--- imagelite/src/app/galeria/page.tsx
+++ imagelite/src/app/galeria/page.tsx
@@ -1,5 +1,17 @@
+import { Template, ImageCard } from '@/components'
+
 export default function GaleriaPage() {
     return(
-        <h1>Galeria</h1>
+        <Template>
+            <section className="grid grid-cols-4 gap-8">
+                <ImageCard nome='Lago' tamanho='10MB' dataUpload='01/01/2025' src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'/>
+                <ImageCard nome='Lago' tamanho='10MB' dataUpload='01/01/2025' src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'/>
+                <ImageCard nome='Lago' tamanho='10MB' dataUpload='01/01/2025' src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'/>
+                <ImageCard nome='Lago' tamanho='10MB' dataUpload='01/01/2025' src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'/>
+                <ImageCard nome='Lago' tamanho='10MB' dataUpload='01/01/2025' src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'/>
+                <ImageCard nome='Lago' tamanho='10MB' dataUpload='01/01/2025' src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'/>
+                <ImageCard nome='Lago' tamanho='10MB' dataUpload='01/01/2025' src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'/>
+            </section>
+        </Template>
     )
 }
