db7aad430c0c31cc79d538fbc1c1058a955bff72
William Mian <william_mian@yahoo.com.br>
2025-01-22 17:04:12 -0300

    formulario e logica pesquisa de imagens

--- imagelite/src/app/galeria/page.tsx
+++ imagelite/src/app/galeria/page.tsx
@@ -9,16 +9,17 @@ export default function GaleriaPage() {
 
     const userService = useImageService();
     const [images, setImages] = useState<Image[]>([]);
+    const [query, setQuery] = useState<string>('');
+    const [extension, setExtension] = useState<string>('');
 
     async function searchImages() {
-        const result = await userService.buscar();
+        const result = await userService.buscar(query, extension);
         setImages(result);
-        console.table(result);
     }
 
     function renderImageCard(image: Image) {
         return (
-            <ImageCard nome={image.name} src={image.url} tamanho={image.size} dataUpload={image.uploadDate}/>
+            <ImageCard key={image.url} nome={image.name} src={image.url} tamanho={image.size} dataUpload={image.uploadDate}/>
         )
     }
 
@@ -28,7 +29,24 @@ export default function GaleriaPage() {
 
     return(
         <Template>
-            <button className="bg-gray-500" onClick={searchImages}>Clique para mudar</button>
+
+            <section className="flex flex-col items-center justify-center my-5">
+                <div className="flex space-x-4">
+                    <input type="text" 
+                           onChange={event => setQuery(event.target.value)}
+                           className="border px-3 py-2 rounded-lg text-gray-900"/>
+                    <select onChange={event => setExtension(event.target.value)}
+                        className="border px-4 py-2 rounded-lg text-gray-900">
+                        <option value="">All formats</option>
+                        <option value="PNG">PNG</option>
+                        <option value="JPEG">JPEG</option>
+                        <option value="GIF">GIF</option>
+                    </select>
+                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={searchImages}>Search</button>
+                    <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg">Add New</button>
+                </div>
+            </section>
+            
             <section className="grid grid-cols-4 gap-8">
                 {
                     renderImageCards()
